a1c5db0adc068762b4930f0aeb3c44ebe710e057
308 <308@academicos.cesde.edu.co>
2022-09-22 07:03:33 -0500

    se crea funcion

--- danielmariaca/funcion1.py
+++ danielmariaca/funcion1.py
@@ -1,2 +1,9 @@
 #FUNCIONES EN PYTHON
-print("Daniel Mariaca")
+
+#DECLARANDO FUNCIONES EN PYTHON
+def saludar(nombre):
+    print(f'Hola: {nombre}')
+
+#UTILIZAR O LLAMAR EN PYTHON
+nombre="Daniel"
+saludar(nombre)
